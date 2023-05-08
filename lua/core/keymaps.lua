0266081dc64560a6440140b892eb81f025788775
rainchill <rainchill2022@foxmail.com>
2023-05-08 21:44:39 +0800

    update

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -17,41 +17,49 @@ keymap.set("n", "<leader>nh", ":nohl<CR>")
 -- nvim-tree
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
 -- 切换buffer
-keymap.set("n", '<C-n>', ":bnext<CR>", {noremap = true});
-keymap.set("n", '<C-p>', ":bprevious<CR>", {noremap = true});
+keymap.set("n", '<C-n>', ":bnext<CR>", { noremap = true });
+keymap.set("n", '<C-p>', ":bprevious<CR>", { noremap = true });
 
 keymap.set("n", "Q", ":q!<CR>")
 keymap.set("n", "S", ":w<CR>", { noremap = true })
 keymap.set("n", "<C-a>", "ggVG")
 
 -- vscode debuy 快捷键
-keymap.set({"i", "n", "v"}, "<F5>", "<cmd>lua require'dap'.continue()<CR>", {silent = true, noremap = true, buffer = bufnr})
-keymap.set({"i", "n", "v"}, "<F10>", "<cmd>lua require'dap'.step_over()<CR>", {silent = true, noremap = true, buffer = bufnr})
-keymap.set({"i", "n", "v"}, "<F11>", "<cmd>lua require'dap'.step_into()<CR>", {silent = true, noremap = true, buffer = bufnr})
-keymap.set({"i", "n", "v"}, "<F12>", "<cmd>lua require'dap'.step_over()<CR>", {silent = true, noremap = true, buffer = bufnr})
-keymap.set({"i", "n", "v"}, "<F9>", "<cmd>lua require'dap'.toggle_breakpoint()<CR>", {silent = true, noremap = true, buffer = bufnr})
+keymap.set({ "i", "n", "v" }, "<F5>", "<cmd>lua require'dap'.continue()<CR>",
+    { silent = true, noremap = true, buffer = bufnr })
+keymap.set({ "i", "n", "v" }, "<F10>", "<cmd>lua require'dap'.step_over()<CR>",
+    { silent = true, noremap = true, buffer = bufnr })
+keymap.set({ "i", "n", "v" }, "<F11>", "<cmd>lua require'dap'.step_into()<CR>",
+    { silent = true, noremap = true, buffer = bufnr })
+keymap.set({ "i", "n", "v" }, "<F12>", "<cmd>lua require'dap'.step_over()<CR>",
+    { silent = true, noremap = true, buffer = bufnr })
+keymap.set({ "i", "n", "v" }, "<F9>", "<cmd>lua require'dap'.toggle_breakpoint()<CR>",
+    { silent = true, noremap = true, buffer = bufnr })
 
 -- 剪切板
-keymap.set("v", "y", "\"+y", {noremap = true})
-keymap.set("v", "p", "\"+p", {noremap = true})
+keymap.set("v", "y", "\"+y", { noremap = true })
+keymap.set("v", "p", "\"+p", { noremap = true })
 
 -- toggleterm
-keymap.set({"n", "t"}, "<c-g>", "<Cmd> ToggleTerm<CR>", {noremap = true, silent = true})
+keymap.set({ "n", "t" }, "<A-\\>", "<Cmd>ToggleTerm direction=horizontal<CR>", { noremap = true, silent = true })
+keymap.set({ "n", "t" }, "<A-d>", "<Cmd>ToggleTerm direction=float<CR>", { noremap = true, silent = true })
 
 -- keymap.set("t", "<esc>", "<c-t><c-n>", {noremap = true, silent = true})
-keymap.set("t", "<c-l>", "<Cmd> wincmd l<CR>", {noremap = true, silent = true})
-keymap.set("t", "<c-h>", "<Cmd> wincmd h<CR>", {noremap = true, silent = true})
-keymap.set("t", "<c-j>", "<Cmd> wincmd j<CR>", {noremap = true, silent = true})
-keymap.set("t", "<c-k>", "<Cmd> wincmd k<CR>", {noremap = true, silent = true})
+keymap.set("t", "<c-l>", "<Cmd> wincmd l<CR>", { noremap = true, silent = true })
+keymap.set("t", "<c-h>", "<Cmd> wincmd h<CR>", { noremap = true, silent = true })
+keymap.set("t", "<c-j>", "<Cmd> wincmd j<CR>", { noremap = true, silent = true })
+keymap.set("t", "<c-k>", "<Cmd> wincmd k<CR>", { noremap = true, silent = true })
 
 -- 移动
-keymap.set("n", "[", "5k", {noremap = true, silent = true});
-keymap.set("n", "]", "5j", {noremap = true, silent = true});
+keymap.set("n", "[", "5k", { noremap = true, silent = true });
+keymap.set("n", "]", "5j", { noremap = true, silent = true });
 
 -- 注释
 -- keymap.set("n", "<c-m>", "gcc", {noremap = true})
 
 -- 模糊查找
-keymap.set("n", "<leader>ff", "<Cmd>Telescope find_files<CR>", {noremap = true, silent = true})
+keymap.set("n", "<leader>ff", "<Cmd>Telescope find_files<CR>", { noremap = true, silent = true })
 
-keymap.set({"n", "v"}, "9", "$", {noremap = true})
+keymap.set({ "n", "v" }, "9", "$", { noremap = true })
+
+keymap.set("n", "lg", ":term lg<CR>", { noremap = true, silent = true })
